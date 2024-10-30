b11ed16edcff2d23eae37e71114e86837552ccb8
thiago-am-souza <thiagosouza96.ts@gmail.com>
2024-10-30 10:40:16 -0300

    implementação de alguns testes

--- README.md
+++ README.md
@@ -1,6 +1,10 @@
 # PlayMove Teste
 
-A aplicação foi desenvolvida utilizando .NET 8, seguindo o padrão de Clean Architecture com o padrão CQRS para separar operações de leitura e gravação. A construção da API foi facilitada pela biblioteca Carter, que simplifica o roteamento e a manipulação das requisições. Para autenticação e autorização, utilizei JWT de maneira simplificada, mas garantindo que apenas usuários autenticados possam acessar funcionalidades. Também utilizei a biblioteca OneOf para tratar respostas e erros de forma mais assertiva.
+A aplicação foi desenvolvida utilizando .NET 8, seguindo o padrão de Clean Architecture com CQRS para separar operações de leitura e gravação. A construção da API foi facilitada pela biblioteca Carter, que simplifica a manipulação das requisições. O PostgreSQL foi a minha opção como banco de dados.
+
+Para autenticação e autorização, foi implementado JWT de maneira simplificada, garantindo que apenas usuários autenticados possam acessar as funcionalidades. 
+
+A aplicação segue os princípios SOLID e adota o DDD para refletir as regras de negócio diretamente nas entidades. A biblioteca OneOf foi utilizada para tratar respostas e erros de domínio de forma mais assertiva. O xUnit foi utilizado para uma implementação basica de testes unitários.
 
 ## Observações
 
@@ -28,3 +32,9 @@ Body para usar na autenticação.
 - Carter
 - JWT
 - OneOf
+- PostgreSQL
+- xUnit
+
+
+
+
---
+++ src/Tests/Fornecedores.Test/Entities/FornecedorTests.cs
@@ -0,0 +1,119 @@
+﻿using Bogus;
+using Bogus.Extensions.Brazil;
+using Fornecedores.Domain.Models;
+using Fornecedores.Domain.ValueObjects;
+
+public class FornecedorTests
+{
+    private readonly Faker _faker;
+
+    public FornecedorTests()
+    {
+        _faker = new Faker("pt_BR");
+    }
+
+    private Fornecedor CriarFornecedorFake()
+    {
+        var cnpj = new Cnpj(_faker.Company.Cnpj());
+        var endereco = new Endereco(_faker.Address.StreetAddress(),
+                                    _faker.Address.Random.AlphaNumeric(2),
+                                    _faker.Address.City(), 
+                                    _faker.Address.State(), 
+                                    _faker.Address.ZipCode());
+
+        return new Fornecedor(
+            _faker.Company.CompanyName(),
+            _faker.Company.CompanySuffix(),
+            _faker.Internet.Email(),
+            cnpj,
+            endereco
+        );
+    }
+
+    [Fact]
+    public void Construtor_Completo_DeveInicializarPropriedadesCorretamente()
+    {        
+        var fornecedor = CriarFornecedorFake();        
+        
+        Assert.NotNull(fornecedor.NomeFantasia);
+        Assert.NotNull(fornecedor.RazaoSocial);
+        Assert.NotNull(fornecedor.Email);
+        Assert.NotNull(fornecedor.Cnpj);
+        Assert.NotNull(fornecedor.Endereco);
+        Assert.False(fornecedor.Desativado);
+    }
+
+    [Fact]
+    public void Ativar_DeveDefinirDesativadoComoFalse()
+    {        
+        var fornecedor = CriarFornecedorFake();
+        fornecedor.Desativar();
+        
+        fornecedor.Ativar();
+        
+        Assert.False(fornecedor.Desativado);
+    }
+
+    [Fact]
+    public void Desativar_DeveDefinirDesativadoComoTrue()
+    {        
+        var fornecedor = CriarFornecedorFake();
+        
+        fornecedor.Desativar();
+        
+        Assert.True(fornecedor.Desativado);
+    }
+
+    [Fact]
+    public void Alterar_DeveModificarPropriedadesCorretamente()
+    {        
+        var fornecedor = CriarFornecedorFake();
+
+        var novoNome = _faker.Company.CompanyName();
+        var novaRazao = _faker.Company.CompanySuffix();
+        var novoEmail = _faker.Internet.Email();
+        var novoCnpj = _faker.Company.Cnpj();
+        
+        fornecedor.Alterar(novoNome, novaRazao, novoEmail, novoCnpj);
+        
+        Assert.Equal(novoNome, fornecedor.NomeFantasia);
+        Assert.Equal(novaRazao, fornecedor.RazaoSocial);
+        Assert.Equal(novoEmail, fornecedor.Email);
+        Assert.Equal(novoCnpj, fornecedor.Cnpj.Numero);
+    }
+
+    [Fact]
+    public void AdicionarTelefone_DeveAdicionarTelefoneNaLista()
+    {        
+        var fornecedor = CriarFornecedorFake();
+        var telefone = new Telefone(_faker.Phone.PhoneNumber("## ####-####"), fornecedor); // Exemplo de telefone
+        
+        fornecedor.AdicionarTelefone(telefone);
+
+        
+        Assert.Contains(telefone, fornecedor.Telefones);
+    }
+
+    [Fact]
+    public void RemoverTelefone_DeveRemoverTelefoneDaLista()
+    {        
+        var fornecedor = CriarFornecedorFake();
+        var telefone = new Telefone(_faker.Phone.PhoneNumber("## ####-####"), fornecedor);
+        fornecedor.AdicionarTelefone(telefone);
+
+        
+        fornecedor.RemoverTelefone(telefone.Id);
+        
+        Assert.DoesNotContain(telefone, fornecedor.Telefones);
+    }
+
+    [Fact]
+    public void Telefones_DeveRetornarIReadOnlyList()
+    {        
+        var fornecedor = CriarFornecedorFake();
+        
+        var telefones = fornecedor.Telefones;
+        
+        Assert.IsAssignableFrom<IReadOnlyList<Telefone>>(telefones);
+    }
+}
